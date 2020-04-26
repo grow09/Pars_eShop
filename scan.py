002bbf0c1124cf56858b79e9fc651daac7392cd2
grow09 <grovexd94@gmail.com>
2020-04-26 20:51:25 +0300

    some refactors

--- scan.py
+++ scan.py
@@ -18,33 +18,29 @@ driver = webdriver.Chrome(executable_path=executable_path)
 
 
 def cycle(id):
-    print("asfa")
     print(id)
-    query = "select count(id) from contributors;"
+    query = "select count(id) from FoxApp_contributor;"
     cursor.execute(query)
     counter = cursor.fetchone()
     while id <= counter[0]:
-        query = "select type from contributors where id = '" + str(id) + "';"
+        query = "select category_id from FoxApp_contributor where id = '" + str(id) + "';"
         cursor.execute(query)
         type_ = cursor.fetchone()
-        print(type_[0])
         try:
-            if type_[0] == "PC":
+            if type_[0] == 1:
                 pc(id)
-            elif type_[0] == "laptop":
+            elif type_[0] == 2:
                 laptop(id)
-            elif type_[0] == "phone":
+            elif type_[0] == 3:
                 smartphone(id)
         except:
             id = id + 1
             cycle(id)
         id = id + 1
 
-
-
 def pc(id):
     id = "'"+str(id)+"'"
-    query = "select link_itbox, link_rozetka, link_citrus, link_allo, link_stylus  from contributors where id = "+id+";"
+    query = "select link_itbox, link_rozetka, link_citrus, link_allo, link_stylus  from FoxApp_contributor where id = "+id+";"
     cursor.execute(query)
     link = cursor.fetchone()
     itbox = link[0]
@@ -58,7 +54,6 @@ def pc(id):
     info = driver.find_elements_by_tag_name("td")
     for elem in info:
         elements1.append(elem.text)
-    print(elements1)
     i = 0
     for elem in elements1:
         i = i+1
@@ -76,36 +71,49 @@ def pc(id):
             hd_type = elements1[i]
         if elem == "Объем HDD" or elem == "Объем SSD":
             hd = elements1[i].split(" ")[0]
-    print(cpu, speed, videocard, ram_type, ram, hd_type, hd)
-    itbox_price = driver.find_element_by_class_name("stuff-price").text.split("грн")[0]
-    print(itbox_price)
-    driver.get(rozetka)
-    driver.implicitly_wait(5)
-    rozetka_price = driver.find_element_by_class_name("product-prices__big").text.split("₴")[0]
-    print(rozetka_price)
-    driver.get(citrus)
-    time.sleep(1)
-    driver.implicitly_wait(5)
-    citrus_price = driver.find_element_by_xpath("//div[@class='price']").text.split("грн")[0]
-    print(citrus_price)
-    driver.get(allo)
-    driver.implicitly_wait(5)
-    allo_price = driver.find_element_by_xpath("//span[@class='price']").text
-    print(allo_price)
-    driver.get(stylus)
-    driver.implicitly_wait(5)
-    stylus_price = driver.find_element_by_class_name("regular-price").text.split("грн")[0]
-    print(stylus_price)
-
-    print(id, cpu, speed, videocard, ram_type, ram, hd_type, hd, itbox_price, rozetka_price, citrus_price, allo_price,
-          stylus_price)
-    print(id.split("'")[1], cpu, speed, videocard, ram_type, ram, hd_type, hd, itbox_price.replace(' ', ''), rozetka_price.replace(' ', ''),
-           citrus_price.replace(' ', ''), allo_price.replace(' ', ''), stylus_price.replace(' ', ''))
-    sql = "INSERT INTO PC (id, cpu, speed, videocard, ram_type, ram, hd_type, hd, price_itbox, price_rozetka, " \
-          "price_citrus, price_allo, price_stylus) " \
-          "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    val = (id.split("'")[1], cpu, speed, videocard, ram_type, ram, hd_type, hd, itbox_price.replace(' ', ''), rozetka_price.replace(' ', ''),
-           citrus_price.replace(' ', ''), allo_price.replace(' ', ''), stylus_price.replace(' ', ''))
+    try:
+        itbox_price = driver.find_element_by_class_name("stuff-price").text.split("грн")[0].replace(' ', '')
+        itbox_price = int(itbox_price)
+    except:
+        itbox_price = 0
+    try:
+        driver.get(rozetka)
+        driver.implicitly_wait(5)
+        rozetka_price = driver.find_element_by_class_name("product-prices__big").text.split("₴")[0].replace(' ', '')
+        rozetka_price = int(rozetka_price)
+    except:
+        rozetka_price = 0
+    try:
+        driver.get(citrus)
+        time.sleep(1)
+        driver.implicitly_wait(5)
+        citrus_price = driver.find_element_by_xpath("//div[@class='price']").text.split("₴")[0].replace(' ', '')
+        citrus_price = int(citrus_price)
+    except:
+        citrus_price = 0
+    try:
+        driver.get(allo)
+        driver.implicitly_wait(5)
+        allo_price = driver.find_element_by_xpath("//meta[@itemprop='price']").get_attribute("content").replace(' ', '')
+        allo_price = int(allo_price)
+    except:
+        allo_price = 0
+    try:
+        driver.get(stylus)
+        driver.implicitly_wait(5)
+        stylus_price = driver.find_element_by_class_name("regular-price").text.split("грн")[0].replace(' ', '')
+        stylus_price = int(stylus_price)
+    except:
+        stylus_price = 0
+    if int(hd) < 10:
+        hd = int(hd)*1000
+    category_id = 1
+    print(id.split("'")[1], cpu, speed, videocard, ram_type, ram, hd_type, hd, itbox_price, rozetka_price, citrus_price, allo_price,
+          stylus_price, category_id)
+    sql = "INSERT INTO FoxApp_pc (id, cpu, speed, videocard, ram_type, ram, hd_type, hd, price_itbox, price_rozetka, " \
+          "price_citrus, price_allo, price_stylus, category_id) " \
+          "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    val = (id.split("'")[1], cpu, speed, videocard, ram_type, ram, hd_type, hd, itbox_price, rozetka_price, citrus_price, allo_price, stylus_price, category_id)
     cursor.execute(sql, val)
     mydb.commit()
 
@@ -114,7 +122,7 @@ def pc(id):
 
 def laptop(id):
     id = "'" + str(id) + "'"
-    query = "select link_itbox, link_rozetka, link_citrus, link_allo, link_stylus  from contributors where id = " + id + ";"
+    query = "select link_itbox, link_rozetka, link_citrus, link_allo, link_stylus  from FoxApp_contributor where id = " + id + ";"
     cursor.execute(query)
     link = cursor.fetchone()
     itbox = link[0]
@@ -128,12 +136,13 @@ def laptop(id):
     info = driver.find_elements_by_tag_name("td")
     for elem in info:
         elements1.append(elem.text)
-    print(elements1)
     i = 0
     for elem in elements1:
         i = i + 1
         if elem == "Процессор":
             proc = elements1[i]
+            cpu = proc.split("(")[0]
+            speed = proc.split("(")[1].replace(')', '').split("ГГц")[0]
         if elem == "Модель видеокарты" or elem == "Видеокарта":
             videocard = elements1[i]
         if elem == "Тип оперативной памяти":
@@ -144,37 +153,49 @@ def laptop(id):
             hd_type = elements1[i]
         if elem == "Объем HDD" or elem == "Объем SSD":
             hd = elements1[i].split(" ")[0]
-    cpu = proc.split("(")[0]
-    speed = proc.split("(")[1].replace(')', '').split("GHz")[0]
-    print(cpu, speed, videocard, ram_type, ram, hd_type, hd)
-    itbox_price = driver.find_element_by_class_name("stuff-price").text.split("грн")[0]
-    print(itbox_price)
-    driver.get(rozetka)
-    driver.implicitly_wait(10)
-    rozetka_price = driver.find_element_by_class_name("product-prices__big").text.split("₴")[0]
-    print(rozetka_price)
-    driver.get(citrus)
-    time.sleep(1)
-    driver.implicitly_wait(10)
-    citrus_price = driver.find_element_by_xpath("//div[@class='price']").text.split("грн")[0]
-    print(citrus_price)
-    driver.get(allo)
-    driver.implicitly_wait(10)
-    allo_price = driver.find_element_by_xpath("//span[@class='price']").text
-    print(allo_price)
-    driver.get(stylus)
-    driver.implicitly_wait(10)
-    stylus_price = driver.find_element_by_class_name("regular-price").text.split("грн")[0]
-    print(stylus_price)
-
-    print(id, cpu, speed, videocard, ram_type, ram, hd_type, hd, itbox_price, rozetka_price, citrus_price, allo_price,
-          stylus_price)
-    sql = "INSERT INTO laptop (id, cpu, speed, videocard, ram_type, ram, hd_type, hd, price_itbox, price_rozetka, " \
-          "price_citrus, price_allo, price_stylus) " \
-          "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    val = (id.split("'")[1], cpu, speed, videocard, ram_type, ram, hd_type, hd, itbox_price.replace(' ', ''),
-           rozetka_price.replace(' ', ''),
-           citrus_price.replace(' ', ''), allo_price.replace(' ', ''), stylus_price.replace(' ', ''))
+    try:
+        itbox_price = driver.find_element_by_class_name("stuff-price").text.split("грн")[0].replace(' ', '')
+        itbox_price = int(itbox_price)
+    except:
+        itbox_price = 0
+    try:
+        driver.get(rozetka)
+        driver.implicitly_wait(5)
+        rozetka_price = driver.find_element_by_class_name("product-prices__big").text.split("₴")[0].replace(' ', '')
+        rozetka_price = int(rozetka_price)
+    except:
+        rozetka_price = 0
+    try:
+        driver.get(citrus)
+        time.sleep(1)
+        driver.implicitly_wait(5)
+        citrus_price = driver.find_element_by_xpath("//div[@class='price']").text.split("₴")[0].replace(' ', '')
+        citrus_price = int(citrus_price)
+    except:
+        citrus_price = 0
+    try:
+        driver.get(allo)
+        driver.implicitly_wait(5)
+        allo_price = driver.find_element_by_xpath("//meta[@itemprop='price']").get_attribute("content").replace(' ', '')
+        allo_price = int(allo_price)
+    except:
+        allo_price = 0
+    try:
+        driver.get(stylus)
+        driver.implicitly_wait(5)
+        stylus_price = driver.find_element_by_class_name("regular-price").text.split("грн")[0].replace(' ', '')
+        stylus_price = int(stylus_price)
+    except:
+        stylus_price = 0
+    if int(hd) < 10:
+        hd = int(hd)*1000
+    category_id = 2
+    print(id.split("'")[1], cpu, float(speed), videocard, ram_type, int(ram), hd_type, hd, itbox_price, rozetka_price, citrus_price, allo_price,
+          stylus_price, category_id)
+    sql = "INSERT INTO FoxApp_laptop (id, cpu, speed, videocard, ram_type, ram, hd_type, hd, price_itbox, price_rozetka, " \
+          "price_citrus, price_allo, price_stylus, category_id) " \
+          "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    val = (id.split("'")[1], cpu, float(speed), videocard, ram_type, int(ram), hd_type, hd, itbox_price, rozetka_price, citrus_price, allo_price, stylus_price, category_id)
     cursor.execute(sql, val)
     mydb.commit()
 
@@ -182,7 +203,7 @@ def laptop(id):
 
 def smartphone(id):
     id = "'" + str(id) + "'"
-    query = "select link_itbox, link_rozetka, link_citrus, link_allo, link_stylus  from contributors where id = " + id + ";"
+    query = "select link_itbox, link_rozetka, link_citrus, link_allo, link_stylus  from FoxApp_contributor where id = " + id + ";"
     cursor.execute(query)
     link = cursor.fetchone()
     itbox = link[0]
@@ -196,7 +217,6 @@ def smartphone(id):
     info = driver.find_elements_by_tag_name("td")
     for elem in info:
         elements1.append(elem.text)
-    print(elements1)
     i = 0
     for elem in elements1:
         i = i + 1
@@ -208,41 +228,55 @@ def smartphone(id):
             ram = elements1[i].split("Gb")[0]
         if elem == "Основная камера":
             back_cam = elements1[i]
+            main_cam = back_cam.split(" ")[0]
         if elem == "Фронтальная камера":
             front_cam = elements1[i]
         if elem == "Операционная система":
             os = elements1[i]
         if elem == "Цвет":
             color = elements1[i]
-    print(cpu, diagonal, back_cam, front_cam, ram, color, os)
-    itbox_price = driver.find_element_by_class_name("stuff-price").text.split("грн")[0]
-    print(itbox_price)
-    driver.get(rozetka)
-    driver.implicitly_wait(5)
-    rozetka_price = driver.find_element_by_class_name("product-prices__big").text.split("₴")[0]
-    print(rozetka_price)
-    driver.get(citrus)
-    time.sleep(1)
-    driver.implicitly_wait(5)
-    citrus_price = driver.find_element_by_xpath("//div[@class='price']").text.split("грн")[0]
-    print(citrus_price)
-    driver.get(allo)
-    driver.implicitly_wait(5)
-    allo_price = driver.find_element_by_xpath("//span[@class='price']").text
-    print(allo_price)
-    driver.get(stylus)
-    driver.implicitly_wait(5)
-    stylus_price = driver.find_element_by_class_name("regular-price").text.split("грн")[0]
-    print(stylus_price)
-
-    print(id, cpu, diagonal, back_cam, front_cam, ram, color, os, itbox_price, rozetka_price, citrus_price, allo_price,
-          stylus_price)
-    sql = "INSERT INTO phone (id, cpu, diagonal, back_cam, front_cam, ram, color, os, price_itbox, price_rozetka, " \
-          "price_citrus, price_allo, price_stylus) " \
-          "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    val = (id.split("'")[1], cpu, diagonal, back_cam, front_cam, ram, color, os, itbox_price.replace(' ', ''),
-           rozetka_price.replace(' ', ''),
-           citrus_price.replace(' ', ''), allo_price.replace(' ', ''), stylus_price.replace(' ', ''))
+    try:
+        itbox_price = driver.find_element_by_class_name("stuff-price").text.split("грн")[0].replace(' ', '')
+        itbox_price = int(itbox_price)
+    except:
+        itbox_price = 0
+    try:
+        driver.get(rozetka)
+        driver.implicitly_wait(5)
+        rozetka_price = driver.find_element_by_class_name("product-prices__big").text.split("₴")[0].replace(' ', '')
+        rozetka_price = int(rozetka_price)
+    except:
+        rozetka_price = 0
+    try:
+        driver.get(citrus)
+        time.sleep(1)
+        driver.implicitly_wait(5)
+        citrus_price = driver.find_element_by_xpath("//div[@class='price']").text.split("₴")[0].replace(' ', '')
+        citrus_price = int(citrus_price)
+    except:
+        citrus_price = 0
+    try:
+        driver.get(allo)
+        driver.implicitly_wait(5)
+        allo_price = driver.find_element_by_xpath("//meta[@itemprop='price']").get_attribute("content").replace(' ', '')
+        allo_price = int(allo_price)
+    except:
+        allo_price = 0
+    try:
+        driver.get(stylus)
+        driver.implicitly_wait(5)
+        stylus_price = driver.find_element_by_class_name("regular-price").text.split("грн")[0].replace(' ', '')
+        stylus_price = int(stylus_price)
+    except:
+        stylus_price = 0
+    category_id = 3
+    print(id.split("'")[1], cpu, float(diagonal), int(main_cam), back_cam, front_cam, ram, color, os, itbox_price,
+           rozetka_price, citrus_price, allo_price, stylus_price, category_id)
+    sql = "INSERT INTO FoxApp_smartphone (id, cpu, diagonal, main_cam, back_cam, front_cam, ram, color, os, price_itbox, price_rozetka, " \
+          "price_citrus, price_allo, price_stylus, category_id) " \
+          "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    val = (id.split("'")[1], cpu, float(diagonal), int(main_cam), back_cam, front_cam, ram, color, os, itbox_price,
+           rozetka_price, citrus_price, allo_price, stylus_price, category_id)
     cursor.execute(sql, val)
     mydb.commit()
 
---
+++ tester_dec.py
@@ -0,0 +1,12 @@
+a = '2.4'
+print(type(a))
+try:
+    it = float(a)
+    print(type(float(a)))
+except:
+    print("asfa")
+# id = 1
+# counter = 1
+
+# while id <= counter:
+#     print("Asda")
